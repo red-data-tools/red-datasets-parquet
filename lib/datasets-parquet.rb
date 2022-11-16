e2eb49200b8f5a1293e779439d99bd2d0e95e853
otegami <a.s.takuya1026@gmail.com>
2022-11-16 22:04:50 +0900

    Added New York city's Taxi and Limousine Commission high volume for hire vehicle trip support

--- lib/datasets-parquet.rb
+++ lib/datasets-parquet.rb
@@ -5,4 +5,5 @@ require_relative "datasets-parquet/version"
 
 require_relative "datasets-parquet/tlc/fhv-trip"
 require_relative "datasets-parquet/tlc/green-taxi-trip"
+require_relative "datasets-parquet/tlc/high-volume-fhv-trip"
 require_relative "datasets-parquet/tlc/yellow-taxi-trip"
